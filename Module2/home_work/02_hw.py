1f6446a724bccd1c374f5a87dce030dd7a46a36d
MattIv28 <96443651+MattIv28@users.noreply.github.com>
2021-12-22 01:11:07 +0400

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -8,3 +8,12 @@
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
 # TODO: your code here
+
+cow = int(input("Введите количество коров "))
+
+if cow % 10 == 1 and cow != 11:
+    print(cow, "корова")
+elif 2 <= cow % 10 <= 4 and cow //10 != 1:
+    print(cow, "коровы")
+else:
+    print(cow, "коров")
