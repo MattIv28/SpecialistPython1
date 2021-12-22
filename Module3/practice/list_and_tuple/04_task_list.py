ad38b064523343d22a2daad99c17027d80d8abf2
MattIv28 <96443651+MattIv28@users.noreply.github.com>
2021-12-22 15:56:06 +0400

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,10 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+
+my_list = [10, 5, 7, 2, -1, 8, -3, 9]
+sum_dig = 0
+for digit in my_list:
+    if digit > 0:
+        sum_dig = sum_dig + digit
+print(sum_dig)
