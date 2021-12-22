9c00be5a573890e640979ffda114e1b0a6b0fede
MattIv28 <96443651+MattIv28@users.noreply.github.com>
2021-12-22 15:50:53 +0400

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+
+my_list = [10, 5, 7, 2, -1, 8, -3, 9]
+sum_dig = 0
+for digit in my_list:
+    sum_dig = sum_dig + digit
+print(sum_dig)
