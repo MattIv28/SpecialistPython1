26023409c3d6d8bca302478f2f0705e5bf37d3cf
MattIv28 <96443651+MattIv28@users.noreply.github.com>
2021-12-22 10:20:11 +0400

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,20 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+
+text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+
+new_text = text.split(" ")
+
+words_per_line = len(new_text)
+#print(words_per_line)
+i = 0
+summ_word = 0
+while i < words_per_line:
+    word_count = len(new_text[i])
+    #print(word_count)
+    if word_count > 5:
+        summ_word = summ_word + 1
+        #print(word_count)
+    i += 1
+print(summ_word)
