a64e88e0c12660e4920d42965f238b2b771f19e7
MattIv28 <96443651+MattIv28@users.noreply.github.com>
2021-12-23 09:53:55 +0400

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,19 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+numbers = []
+sum_dig = 0
+n = int(input("Введите число элементов списка: "))
+
+i = 0
+while i < n:
+    x = random.randint(-100, 100)
+    numbers.insert(i, x)
+    i += 1
+print(numbers)
+for digit in numbers:
+    if digit > 0 and digit % 2 == 0:
+        sum_dig = sum_dig + digit
+print(sum_dig)
